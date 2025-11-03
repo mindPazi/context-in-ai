da7678a72e172a55854cdc80773842f821c3d08e
mindPazi <gitmind87@gmail.com>
2025-11-03 06:22:48 +0100

    Fixed escape sequence for dumping methods

--- src/main/java/com/github/mindpazi/contextinaitool/dump/JsonDumper.java
+++ src/main/java/com/github/mindpazi/contextinaitool/dump/JsonDumper.java
@@ -13,7 +13,10 @@ import java.util.Set;
 
 public class JsonDumper {
     public static void dump(Set<MethodMeta> methods, Project project) throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Gson gson = new GsonBuilder()
+                .disableHtmlEscaping()
+                .setPrettyPrinting()
+                .create();
         String json = gson.toJson(methods);
         Path outputPath = Paths.get(project.getBasePath(), "methods.json");
         Files.writeString(outputPath, json);
--- src/main/java/com/github/mindpazi/contextinaitool/model/MethodsPerFileValue.java
+++ src/main/java/com/github/mindpazi/contextinaitool/model/MethodsPerFileValue.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.Set;
 
 public record MethodsPerFileValue(
-        Set<MethodMeta> methods
-) implements Serializable {
+        Set<MethodMeta> methods) implements Serializable {
     private static final long serialVersionUID = 1L;
 }
